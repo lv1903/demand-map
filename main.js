231f9834027c21feed502c254c84ddacbe237826
lv1903 <leovalberg@gmail.com>
2016-02-11 16:12:34 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,422 @@
+var express = require("express");
+var jade = require("jade");
+var bodyParser = require("body-parser");
+var app = express();
+var aMapStyles = require("./public/stylesheets/styledMap.json");
+var aMapStylesLocal = require("./public/stylesheets/styledMapLocal.json");
+
+var oGeoLSOA = require("./data/geoLSOA.json");
+var oPopData = require("./data/popData.json");
+
+
+var oMaps = [
+
+
+    {
+        id: "gpdemand",
+        header: "Hampshire GP Demand Map",
+        demandUnit: "consultations",
+        rateFile: "gp_rates.json",
+        rateData: {},
+        demandData: {},
+        unitCostPounds: 45,
+        supplyPointsQuery: "http://q.nqminds.com/v1/datasets/EkgAN_mfLx/data",
+        supplyPointSchema: {
+            name: ["name"],
+            facts: [
+                ["registered_patients"],
+                ["nhs_choices_user_rating"]
+            ],
+            address: [
+                ["address"],
+                ["post_code"]
+            ],
+            lat: ["geoCode", "lat"],
+            lng: ["geoCode", "lng"]
+        }
+    },
+
+
+    {
+        id: "primarydemand",
+        header: "Hampshire Primary School Demand Map",
+        demandUnit: "primary school places",
+        rateFile: "primary_rates.json",
+        rateData: {},
+        demandData: {},
+        unitCostPounds: 16768,
+        supplyPointQuery: "http://q.nqminds.com/v1/datasets/NkWEN1DTtl/data?filter={%22properties.PhaseOfEducation%22:%22Primary%22}",
+        supplyPointSchema: {
+            name: ["properties", "EstablishmentName"],
+            facts: [
+                ["properties", "HeadFirstName"],
+                ["properties", "HeadLastName"],
+                ["properties", "HeadPreferredJobTitle"],
+                ["properties", "TypeOfEstablishment"]
+            ],
+            address: [
+                ["properties", "Street"],
+                ["properties", "Town"],
+                ["properties", "Postcode"]
+            ],
+            lat: ["geometry", "coordinates", 1],
+            lng: ["geometry", "coordinates", 0]
+        }
+    },
+
+    {
+        id: "secondarydemand",
+        header: "Hampshire Secondary School Demand Map",
+        demandUnit: "secondary school places",
+        rateFile: "secondary_rates.json",
+        rateData: {},
+        demandData: {},
+        unitCostPounds: 16768,
+        supplyPointQuery: "http://q.nqminds.com/v1/datasets/NkWEN1DTtl/data?filter={%22properties.PhaseOfEducation%22:%22Secondary%22}",
+        supplyPointSchema: {
+            name: ["properties", "EstablishmentName"],
+            facts: [
+                ["properties", "HeadFirstName"],
+                ["properties", "HeadLastName"],
+                ["properties", "HeadPreferredJobTitle"],
+                ["properties", "TypeOfEstablishment"]
+            ],
+            address: [
+                ["properties", "Street"],
+                ["properties", "Town"],
+                ["properties", "Postcode"]
+            ],
+            lat: ["geometry", "coordinates", 1],
+            lng: ["geometry", "coordinates", 0]
+        }
+    },
+
+    {
+        id: "collegedemand",
+        header: "Hampshire College Demand Map",
+        demandUnit: "college places",
+        rateFile: "college_rates.json",
+        rateData: {},
+        demandData: {},
+        unitCostPounds: 16768,
+        supplyPointQuery: "http://q.nqminds.com/v1/datasets/NkWEN1DTtl/data?filter={%22properties.PhaseOfEducation%22:%22Secondary%22,%20%22properties.OfficialSixthForm%22:%22Has%20a%20sixth%20form%22}",
+        supplyPointSchema: {
+            name: ["properties", "EstablishmentName"],
+            facts: [
+                ["properties", "HeadFirstName"],
+                ["properties", "HeadLastName"],
+                ["properties", "HeadPreferredJobTitle"],
+                ["properties", "TypeOfEstablishment"]
+            ],
+            address: [
+                ["properties", "Street"],
+                ["properties", "Town"],
+                ["properties", "Postcode"]
+            ],
+            lat: ["geometry", "coordinates", 1],
+            lng: ["geometry", "coordinates", 0]
+        }
+    },
+
+
+    {
+        id: "caredemand",
+        header: "Hampshire Elderly Residential Care Demand Map",
+        demandUnit: "residents in care",
+        rateFile: "care_rates.json",
+        rateData: {},
+        demandData: {},
+        unitCostPounds: 20000,
+        supplyPointQuery: "http://q.nqminds.com/v1/datasets/EyW56eH2Ke/data",
+        supplyPointSchema: {
+            name: ["properties","Name"],
+            facts:[
+                ["properties","Provider name"],
+                ["properties","Service types"]
+            ],
+            address: [
+                ["properties","Address"],
+                ["properties","Postcode"]
+            ],
+            lat: ["geometry","coordinates",1],
+            lng: ["geometry","coordinates",0]
+
+        }
+    }
+
+];
+
+
+//var oConsultationData = require("./data/consultationRates.json");
+var oGPsData = require("./data/gpSurgeries.json");
+
+
+var baseYear = "2015";
+var homeYear = "2017";
+
+app.set("views", __dirname + "/views");
+app.set("view engine","jade");
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({limit: (5*1024*1000) }));
+
+function calculatePressure(oPopData, oRateData){
+
+    var oDemandData = {};
+    var id;
+    for(id in oPopData){
+        var year;
+        for(year in oPopData[id]){
+            var sumVisits = 0;
+            if(!(oDemandData.hasOwnProperty(year))){oDemandData[year] = {}}
+            if(!(oDemandData[year].hasOwnProperty(id))){oDemandData[year][id] = {}}
+            var sex;
+            for (sex in oPopData[id][year]){
+                var age;
+                for (age in oPopData[id][year][sex]){
+                    sumVisits += oPopData[id][year][sex][age] * oRateData["rates"][sex][age];
+                }
+            }
+            oDemandData[year][id] = sumVisits
+        }
+        var baseValue = oDemandData[baseYear][id]
+        for(year in oDemandData){
+            oDemandData[year][id] = oDemandData[year][id] / baseValue;
+        }
+    }
+    return oDemandData
+}
+
+
+function nqm_tbx_query(options, callback){
+    //requests data from tbx and returns body string
+
+    var http = require("http");
+    var req = http.get(options, function(res) {
+
+        var bodyChunks = [];
+        res.on('data', function(chunk) {
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+            callback(i, body)
+        })
+    });
+    req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+};
+
+
+app.get('/references', function(req, res){
+    res.render('references');
+});
+
+
+app.get("/:mapId/pressure_data/:year/", function(req, res){
+
+
+    var mapId = req.params["mapId"];
+    var obj = oMaps.filter(function(el){return el.id == mapId})[0];
+
+    var year = req.params["year"];
+
+    //console.log(mapId + " " + year)
+    //console.log(obj.demandData[year])
+    res.json(obj.demandData[year]);
+});
+
+app.get("/:mapId/cost_data/:id/:year", function(req, res){
+
+
+    var mapId = req.params["mapId"];
+    var obj = oMaps.filter(function(el){return el.id == mapId})[0];
+
+    var id = req.params["id"];
+    var year = req.params["year"];
+
+    //console.log(obj.rateData.rates);
+
+
+    var sex;
+    var age;
+    var baseCost = 0;
+    var cost = 0;
+
+    //get base cost
+    for (sex in oPopData[id][baseYear]){
+        for (age in oPopData[id][baseYear][sex]){
+            baseCost += oPopData[id][baseYear][sex][age] * obj.rateData.rates[sex][age] * obj.unitCostPounds;
+        }
+    }
+
+
+    //get year cost
+    for (sex in oPopData[id][year]){
+        for (age in oPopData[id][year][sex]){
+            cost += oPopData[id][year][sex][age] * obj.rateData.rates[sex][age] * obj.unitCostPounds;
+        }
+    }
+
+    res.json({"baseCost": baseCost, "cost": cost})
+
+
+    //console.log(oPopData[id])
+    //
+    //res.json(oPopData[id]);
+});
+
+
+app.get("/:mapId/demand_data/:id/:year", function(req, res){
+
+    var mapId = req.params["mapId"];
+    var obj = oMaps.filter(function(el){return el.id == mapId})[0];
+
+    var id = req.params["id"];
+    var year = req.params["year"];
+
+    var data = [];
+    var sex;
+    var age;
+
+
+    var data_obj = {};
+
+    pop = 0;
+
+
+    for (sex in oPopData[id][year]){
+        for (age in oPopData[id][year][sex]) {
+            if(!data_obj.hasOwnProperty(age)){
+                data_obj[age] = {};
+                data_obj[age].age = age
+
+            }
+            data_obj[age][sex] = oPopData[id][year][sex][age] * obj.rateData.rates[sex][age]
+        }
+    }
+
+    for(age in data_obj){
+        data_obj[age].age = data_obj[age].age.replace("-to", ""); //change text for plot labels
+        data_obj[age].age = data_obj[age].age.replace("-and-over", "+"); //change text for plot labels
+        data.push(data_obj[age])
+    }
+
+    data.reverse();
+
+    res.json(data);
+
+
+});
+
+
+app.get("/pop_data/:id/:year", function(req, res){
+
+
+    var id = req.params["id"];
+    var year = req.params["year"];
+
+    var data = [];
+    var sex;
+    var age;
+
+
+    var data_obj = {};
+
+    pop = 0;
+
+
+    for (sex in oPopData[id][year]){
+        for (age in oPopData[id][year][sex]) {
+            if(!data_obj.hasOwnProperty(age)){
+                data_obj[age] = {};
+                data_obj[age].age = age
+
+            }
+            data_obj[age][sex] = oPopData[id][year][sex][age]
+        }
+    }
+
+    for(age in data_obj){
+        data_obj[age].age = data_obj[age].age.replace("-to", ""); //change text for plot labels
+        data_obj[age].age = data_obj[age].age.replace("-and-over", "+"); //change text for plot labels
+        data.push(data_obj[age])
+    }
+
+    data.reverse();
+
+    res.json(data);
+
+
+});
+
+
+app.get("/total_pop/:id/", function(req, res){
+
+    var id = req.params["id"];
+
+    var data = [];
+    var year;
+    var sex;
+    var age;
+    var pop;
+    var i = 0;
+    for(year in oPopData[id]) {
+        pop = 0;
+        for (sex in oPopData[id][year]){
+            for (age in oPopData[id][year][sex]) {
+                pop += Number(oPopData[id][year][sex][age])
+            }
+        }
+        data.push({id: i, year: year, total_pop: pop});
+        i++
+    }
+
+
+
+    res.json(data);
+});
+
+
+
+
+app.get('/:mapId', function(req, res){
+
+    var mapId = req.params["mapId"];
+
+    var obj = oMaps.filter(function(el){return el.id == mapId})[0];
+
+
+    res.render('webix', {
+                        title: mapId
+                        , header: obj.header
+                        , mapId: mapId
+                        , oMap: obj
+                        , year: homeYear
+                        , demandUnit: obj.demandUnit
+                        , mapStyle: aMapStyles
+                        , mapStyleLocal: aMapStylesLocal
+                        , geoData: oGeoLSOA
+                        , demandDataYear: obj.demandData[homeYear]
+                        , rateData: obj.rateData
+                        }
+    );
+});
+
+
+
+
+for(var i in oMaps){
+
+    var obj = oMaps[i];
+    obj.rateData = require("./data/" + obj.rateFile);
+    obj.demandData = calculatePressure(oPopData, obj.rateData);
+
+
+
+
+}
+
+//var odemandData = calculatePressure(oPopData, oConsultationData);
+
+
+app.listen(3021);
---
+++ public/js/buttons.js
@@ -0,0 +1,234 @@
+function findYearIndex(){
+    var i = 0;
+    while($$("yearList").getValue() != aYears[i].value){
+        i++;
+    }
+    return i
+}
+
+function addPolygonColors(oPressureDataYear){
+
+    $$('map').map.data.setStyle(function(feature) {
+        var fid = feature.getProperty('LSOA01CD');
+
+        if(typeof fid != "string"){
+            var fid = feature.getProperty('LSOA11CD');
+        }
+        if (!oPressureDataYear.hasOwnProperty(fid)) {
+            return {
+                fillColor: selectKeyColor("NA"),
+                fillOpacity: 0.7,
+                strokeWeight: 0.5,
+                strokeOpacity: 0.7,
+                strokeColor: "black"
+            }
+        }else if(fid ==id){
+            var n = oPressureDataYear[fid];
+            var color = selectKeyColor(n);
+            return {
+                fillColor: color,
+                fillOpacity: 0.7,
+                strokeWeight: 3,
+                strokeOpacity: 1,
+                strokeColor: "blue"
+            }
+
+        } else {
+            var n = oPressureDataYear[fid];
+            var color = selectKeyColor(n);
+            return {
+                fillColor: color,
+                fillOpacity: 0.7,
+                strokeWeight: 0.5,
+                strokeOpacity: 0.7,
+                strokeColor: "black"
+            }
+        }
+    })
+
+}
+
+
+
+
+function polygonColors(year){
+    if(oDemandData.hasOwnProperty(year)) {
+        addPolygonColors((oDemandData[year]))
+    } else {
+
+        webix.ajax("/" + mapId + "/pressure_data/" + year, function(text,data){
+            //text = '{ some :"abc"}'
+            oDemandData[year] = (data.json()); //abc
+            addPolygonColors((oDemandData[year]))
+        });
+    }
+}
+
+var backYear = function(){
+
+    var year;
+
+    var i = findYearIndex();
+    if( i == 0) {
+        year = aYears[0].value
+    }else {
+        year = aYears[i - 1].value;
+        $$("yearList").setValue(year);
+    }
+
+    updateYear(year);
+
+};
+
+var forwardYear = function(){
+
+    var year;
+
+    var i = findYearIndex();
+    if( i == aYears.length - 1) {
+        year = aYears[i].value
+    }else {
+        year = aYears[i + 1].value;
+        $$("yearList").setValue(year);
+    }
+
+    updateYear(year);
+
+
+};
+
+var selectYear = function(){
+    year = getYear();
+    updateYear();
+
+};
+
+var updateYear = function(){
+
+    polygonColors(year);
+
+    //col  1
+    updateCost();
+    $$("areaYear").setHTML("<div style='font-size: 42px; padding: 14px; color: lightgrey; font-weight: bold'>" + year + "</div>")
+
+
+    //col 2
+    updateDemandGraph();
+    updatePopPyramidGraph();
+
+    //col 3
+    updateLineGraph();
+
+
+};
+
+var getYear = function(){
+    return $$("yearList").getValue();
+};
+
+var updateFeature = function(name){
+    $$("areaName").setHTML("<div>Local Area: " + name + "</div>");
+};
+
+
+var updateCost = function(){
+    webix.ajax("/" + mapId + "/cost_data/" + id + "/"  + year , function(text,data){
+        $$("areaCost").setHTML(
+            "<br><div style='font-size: 35px; padding: 14px; line-height: 20%'>" + ( data.json().cost - data.json().baseCost).toMoney(0) + "</div>" +
+            "<p style='font-size: 14px; '>Annual cost increase from 2015 for local area</p>"
+        )
+    });
+};
+
+var updateLineGraph = function(){
+    webix.ajax("/total_pop/" + id, function(text,data){
+        //console.log(data.json())
+
+        $$("popChart").data.clearAll();
+
+        for(i in data.json()){
+            $$("popChart").data.add(data.json()[i])
+        }
+
+        $$("popChart").attachEvent("onItemClick", function(id){
+            year = this.getItem(id).year;
+            $$("yearList").setValue(year);
+            updateYear();
+        })
+    });
+};
+
+
+var updateDemandGraph = function(){
+    webix.ajax("/" + mapId + "/demand_data/" + id + "/"  + year , function(text,data){
+        //console.log(data.json())
+
+        $$("demandChart").data.clearAll();
+
+        for(i in data.json()){
+            $$("demandChart").data.add(data.json()[i])
+        }
+    });
+};
+
+var updatePopPyramidGraph = function(){
+    webix.ajax("/pop_data/" + id + "/"  + year , function(text,data){
+        //console.log(data.json())
+
+        $$("popPyramidChart").data.clearAll();
+
+        for(i in data.json()){
+            $$("popPyramidChart").data.add(data.json()[i])
+        }
+    });
+};
+
+
+
+function featureClick(event){
+
+    $$("graphView").expand();
+
+
+
+    id = event.feature.getProperty('LSOA01CD');
+    var name = event.feature.getProperty('LSOA01NM');
+    year = getYear();
+
+    //col 1
+    updateCost();
+    updateYear();
+    updateFeature(name);
+
+    //col 2
+    updateDemandGraph();
+    updatePopPyramidGraph();
+
+    //col 3
+    updateLineGraph();
+
+}
+
+
+
+
+Number.prototype.toMoney = function(decimals, decimal_sep, thousands_sep){
+    var n = this,
+        c = isNaN(decimals) ? 2 : Math.abs(decimals), //if decimal is zero we must take it, it means user does not want to show any decimal
+        d = decimal_sep || '.', //if no decimal separator is passed we use the dot as default decimal separator (we MUST use a decimal separator)
+
+    /*
+     according to [http://stackoverflow.com/questions/411352/how-best-to-determine-if-an-argument-is-not-sent-to-the-javascript-function]
+     the fastest way to check for not defined parameter is to use typeof value === 'undefined'
+     rather than doing value === undefined.
+     */
+        t = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep, //if you don't want to use a thousands separator you can pass empty string as thousands_sep value
+
+        sign = (n < 0) ? '-' : '',
+
+    //extracting the absolute value of the integer part of the number and converting to string
+        i = parseInt(n = Math.abs(n).toFixed(c)) + '',
+
+        j = ((j = i.length) > 3) ? j % 3 : 0;
+    return sign + " &pound " + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : '');
+};
